1dd3a8aa7d6eb57c836d14755b1c077a859831fc
saraorsi <saraorsi@Saras-MBP.lan>
2024-10-19 23:26:47 +0100

    add props card

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,7 +1,31 @@
 interface CardProps {
+  date: string;
   title: string;
+  exercept?: string;
+  type: string;
+  categories?: string;
+  tags?: string;
+  link?: string;
 }
 
-export default function Card({ title }: CardProps) {
-  return <article>{title}</article>;
+export default function Card({
+  date,
+  title,
+  exercept,
+  type,
+  categories,
+  tags,
+  link,
+}: CardProps) {
+  return (
+    <article>
+      <div>{date}</div>
+      <div>{title}</div>
+      <div>{exercept}</div>
+      <div>{type}</div>
+      <div>{categories}</div>
+      <div>{tags}</div>
+      <div>{link}</div>
+    </article>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,18 @@ export default async function Page() {
         if (i === 0) {
           return <div key={i}></div>;
         } else {
-          return <Card key={i} title={item[1]} />;
+          return (
+            <Card
+              key={i}
+              date={item[0]}
+              title={item[1]}
+              exercept={item[2]}
+              type={item[3]}
+              categories={item[4]}
+              tags={item[5]}
+              link={item[6]}
+            />
+          );
         }
       })}
     </div>
